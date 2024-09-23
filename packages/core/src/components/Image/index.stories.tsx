b5ab9ac3bbb5151d0a915886d8542696d3784bdd
webblocksapp <mauriver21@gmail.com>
2024-09-22 21:55:31 -0500

    Update image component

--- packages/core/src/components/Image/index.stories.tsx
+++ packages/core/src/components/Image/index.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Image } from '@components';
+import { Box, Image, Stack } from '@components';
+import { useState } from 'react';
 
 const meta: Meta = {
   title: 'Components/Image',
@@ -11,8 +12,48 @@ export default meta;
 type Story = StoryObj<typeof Image>;
 
 export const Overview: Story = {
-  args: {
-    src: 'https://petapixel.com/assets/uploads/2022/08/fdfs19.jpg',
-    width: 200,
+  render: () => {
+    const [imageError, setImageError] = useState('');
+
+    return (
+      <Stack spacing={2}>
+        <Image
+          src="https://petapixel.com/assets/uploads/2022/08/fdfs19.jpg"
+          width={200}
+        />
+        <Box>
+          {imageError}
+          <Image
+            onImageError={(message) => setImageError(message)}
+            src="https://brokenImageUrl"
+            width={200}
+          />
+        </Box>
+        <Box
+          width={300}
+          height={300}
+          border={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image showError src="https://brokenImageUrl" width={200} />
+        </Box>
+        <Box
+          width={300}
+          height={300}
+          border={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image
+            showLoading
+            src="https://vastphotos.com/files/uploads/photos/11809/spring-green-hills-vast-xl.jpg?v=20240503092916"
+            width="100%"
+          />
+        </Box>
+      </Stack>
+    );
   },
 };
--- packages/core/src/components/Image/index.tsx
+++ packages/core/src/components/Image/index.tsx
@@ -1,5 +1,73 @@
-export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
+import { useEffect, useRef, useState } from 'react';
+import { Body2, Box, CircularProgress } from '@components';
 
-export const Image: React.FC<ImageProps> = (props) => {
-  return <img {...props} />;
+export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  onImageError?: (message: string) => void;
+  showLoading?: boolean;
+  showError?: boolean;
+  failedErrorMessage?: string;
+}
+
+export const Image: React.FC<ImageProps> = ({
+  onImageError,
+  showLoading,
+  style,
+  showError,
+  failedErrorMessage = 'Failed to load image',
+  ...rest
+}) => {
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [loading, setLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  const onLoad = () => {
+    onImageError?.('');
+    setLoading(false);
+  };
+
+  const onError = () => {
+    setHasError(true);
+    onImageError?.(failedErrorMessage);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const img = imgRef.current;
+    if (img === null) return;
+
+    img.addEventListener('load', onLoad);
+    img.addEventListener('error', onError);
+
+    return () => {
+      img.removeEventListener('load', onLoad);
+      img.removeEventListener('error', onError);
+    };
+  }, []);
+
+  return (
+    <div>
+      {showLoading && loading ? (
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <CircularProgress color="info" />
+        </Box>
+      ) : (
+        <></>
+      )}
+      {hasError && showError ? (
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Body2 fontWeight={500}>{failedErrorMessage}</Body2>
+        </Box>
+      ) : (
+        <></>
+      )}
+      <img
+        ref={imgRef}
+        style={{
+          ...style,
+          ...(loading || (hasError && showError) ? { display: 'none' } : {}),
+        }}
+        {...rest}
+      />
+    </div>
+  );
 };
