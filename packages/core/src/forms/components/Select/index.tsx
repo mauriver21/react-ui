4469ddc2f152da52acc739d752b211f4fcea7262
Mauricio Rivera <mauriver21@gmail.com>
2025-08-15 21:25:20 -0500

    Refactor Select

--- packages/core/src/forms/components/Select/index.tsx
+++ packages/core/src/forms/components/Select/index.tsx
@@ -1,11 +1,68 @@
 import { FieldProps } from '@forms/interfaces/FieldProps';
 import {
-  Select as BaseSelect,
-  SelectProps as BaseSelectProps,
-} from 'reactjs-ui-core';
+  Select as MuiSelect,
+  SelectProps as MuiSelectProps,
+} from '@main/components/Select';
 import { Field } from '@forms/lib/Field';
+import { withDefaultProps } from '@main/hocs/withDefaultProps';
+import { Box, BoxProps } from '@main/components/Box';
+import { FormControl } from '@main/components/FormControl';
+import { InputLabel } from '@main/components/InputLabel';
+import { SelectableOption } from '@main/interfaces/SelectableOption';
+import { MenuItem } from '@main/components/MenuItem';
+import { FormHelperText } from '@main/components/FormHelperText';
 
-export type SelectProps = BaseSelectProps & FieldProps;
+export type SelectProps = MuiSelectProps & {
+  rootSx?: BoxProps['sx'];
+  defaultRootSx?: BoxProps['sx'];
+  options?: Array<SelectableOption>;
+  helperText?: string;
+} & FieldProps;
+
+const BaseSelect = withDefaultProps(
+  ({
+    rootSx,
+    defaultRootSx,
+    helperText,
+    options = [],
+    error,
+    fullWidth = true,
+    errorMessage,
+    ...rest
+  }: SelectProps) => {
+    const sx = { ...defaultRootSx, ...rootSx };
+
+    return (
+      <Box sx={sx}>
+        <FormControl fullWidth>
+          {rest.label && (
+            <InputLabel
+              id={rest.labelId}
+              error={error}
+              disabled={rest.disabled}
+            >
+              {rest.label}
+            </InputLabel>
+          )}
+          <MuiSelect fullWidth={fullWidth} error={error} {...rest}>
+            {options.map((option, index) => (
+              <MenuItem value={option.value} key={index}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </MuiSelect>
+        </FormControl>
+        {helperText && (
+          <FormHelperText disabled={rest.disabled}>{helperText}</FormHelperText>
+        )}
+        {errorMessage && (
+          <FormHelperText error={error}>{errorMessage}</FormHelperText>
+        )}
+      </Box>
+    );
+  },
+  'Select'
+);
 
 export const Select: React.FC<SelectProps> = ({
   fullWidth = true,
--- packages/core/src/main/components/Select/index.tsx
+++ packages/core/src/main/components/Select/index.tsx
@@ -1,74 +1,10 @@
-import { Box, BoxProps } from '@main/components/Box';
-import { FormControl } from '@main/components/FormControl';
-import { FormHelperText } from '@main/components/FormHelperText';
-import { InputLabel } from '@main/components/InputLabel';
-import { MenuItem } from '@main/components/MenuItem';
-import { withDefaultProps } from '@main/hocs/withDefaultProps';
-import { SelectableOption } from '@main/interfaces/SelectableOption';
 import {
   Select as MuiSelect,
   SelectProps as MuiSelectProps,
 } from '@mui/material';
-import { useCallback } from 'react';
 
-export type SelectProps = MuiSelectProps & {
-  rootSx?: BoxProps['sx'];
-  defaultRootSx?: BoxProps['sx'];
-  helperText?: string;
-  options?: Array<SelectableOption>;
-  errorMessage?: string;
-};
+export type SelectProps = MuiSelectProps;
 
-export const Select: React.FC<SelectProps> = (props) => {
-  const Component = useCallback(
-    withDefaultProps(
-      ({
-        rootSx,
-        defaultRootSx,
-        helperText,
-        options = [],
-        error,
-        fullWidth = true,
-        errorMessage,
-        ...rest
-      }: SelectProps) => {
-        const sx = { ...defaultRootSx, ...rootSx };
-
-        return (
-          <Box sx={sx}>
-            <FormControl fullWidth>
-              {rest.label && (
-                <InputLabel
-                  id={rest.labelId}
-                  error={error}
-                  disabled={rest.disabled}
-                >
-                  {rest.label}
-                </InputLabel>
-              )}
-              <MuiSelect fullWidth={fullWidth} error={error} {...rest}>
-                {options.map((option, index) => (
-                  <MenuItem value={option.value} key={index}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </MuiSelect>
-            </FormControl>
-            {helperText && (
-              <FormHelperText disabled={rest.disabled}>
-                {helperText}
-              </FormHelperText>
-            )}
-            {errorMessage && (
-              <FormHelperText error={error}>{errorMessage}</FormHelperText>
-            )}
-          </Box>
-        );
-      },
-      'Select'
-    ),
-    []
-  );
-
-  return <Component {...props} />;
-};
+export const Select: React.FC<SelectProps> = (props) => (
+  <MuiSelect {...props} />
+);
