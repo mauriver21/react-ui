0a6dc44da8a5095616cf57c5b2aabe4aba1ff1c1
Mauricio Rivera <mauriver21@gmail.com>
2025-08-16 17:25:01 -0500

    Test components in sandbox

--- packages/sandbox/src/App.tsx
+++ packages/sandbox/src/App.tsx
@@ -1,7 +1,15 @@
-import { Body1 } from 'reactjs-shared-ui';
+import { Box, Stack } from 'reactjs-shared-ui';
+import { Select, TextField } from 'reactjs-shared-ui/forms';
 
 function App() {
-  return <Body1>Hello</Body1>;
+  return (
+    <Box p={2}>
+      <Stack spacing={1}>
+        <TextField label="Name" />
+        <Select options={[{ value: 1, label: 'Pizza' }]} />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
